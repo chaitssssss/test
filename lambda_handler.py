47a0d30f0a294ec01ef15187701a824da89a6370
chaitssssss <chaituthegoodboy@gmail.com>
2024-07-27 18:41:12 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,102 +1,74 @@
-import json
-import yaml
-import os
-import boto3
-from boto3.dynamodb.conditions import Key
-
-
-
-region = os.environ.get("AWS_REGION")
-current_dir = os.getcwd()
-print(current_dir)
-file_path = os.path.join(current_dir, 'config.yaml')
-
-
-
 def load_yaml(path):
-    with open(path,'r',encoding='utf-8') as file:
+    with open(path, 'r', encoding='utf-8') as file:
         data = yaml.safe_load(file)
-        print(data)
         return data
 
+"""
+get all the mandatory job groups from the config file
+"""
 
-def get_other_groups_in_job_group(data,group_name):
-    other_groups = []
-    for job_group, groups in data['QA'].items():
-        if group_name in groups:
-            for group in groups:
-                if group != group_name:
-                    other_groups.append(group)
-            break
-    return other_groups
+def get_mandatory_job_groups(config_data):
+    mandatory_job_groups = []
+    job_groups = config_data['QA']['JOB_GROUP_MANDATORY']
+    
+    for group, datasets in job_groups.items():
+        if group == 'IS_MANDATORY':
+            continue
+        mandatory_job_groups.append(group)
+    
+    return mandatory_job_groups
 
 
-
-def query_dynamo_db_job_status(table_name,job_id):
-    dynamodb_con = boto3.resource("dynamodb",region_name=region)
-    table = dynamodb_con.table('job_status_table')
+"""
+Interface to connect to dynamo db and retrieve the data based on job id
+"""
+def query_dynamo_db_job_status(table_name, job_id):
+    dynamodb_con = boto3.resource("dynamodb", region_name=region)
+    table = dynamodb_con.Table(table_name)
     key_condition_expression = Key('job_id').eq(job_id)
 
-    # Perform the query
     response = table.query(
-        TableName=table_name,
         KeyConditionExpression=key_condition_expression
     )
     return response
 
+"""
+    checking each group in the job_group_mandatory is successful or not
+"""
+def check_mandatory_jobs_success(mandatory_job_groups):
+    all_successful = True
+    responses = {}
+    today_date_str = datetime.now().strftime('%Y-%m-%d')
 
+    for group_name in mandatory_job_groups:
+        job_id = f"{group_name}-{today_date_str}"
+        response = query_dynamo_db_job_status('batch_job_status', job_id)
+        responses[job_id] = response['Items']
+        if len(response['Items']) == 0 or response['Items'][0]['job_status'] != 'SUCCESS':
+            all_successful = False
+    
+    return all_successful, responses
 
 def lambda_handler(event, context):
     try:
-        responses = {}
-        # Check if 'Records' is in the event
-        if 'Records' not in event:
-            raise ValueError("Event does not contain 'Records'")
+        config_data = load_yaml(file_path)
+        mandatory_job_groups = get_mandatory_job_groups(config_data)
+        
+        all_successful, responses = check_mandatory_jobs_success(mandatory_job_groups)
 
-        # Check if 'Records' is empty
-        if not event['Records']:
-            raise ValueError("Event contains empty 'Records'")
+        if all_successful:
+            print("All mandatory jobs are successful.")
+        else:
+            print("Some mandatory jobs are not successful.")
 
-        for record in event['Records']:
-
-            keys = record['dynamodb']['Keys']
-            job_id = keys['job_id']['S']
-            job_id_meta = job_id.split("-")
-            group_name  = job_id_meta[0].upper
-            date_str = job_id_meta[1]
-
-            job_start_utc_timestamp = keys['job_start_utc_timestamp']['N']
-            new_image = record['dynamodb']['NewImage']
-            job_status = new_image['job_status']['S']
-
-
-            if job_status == 'SUCCESS':
-                data = load_yaml(file_path)
-                group_names = get_other_groups_in_job_group(data,group_name)
-                job_id_key_list = []
-                for group in group_names:
-                    job_id_partition_key = f"{group}-{date_str}"
-                    job_id_key_list.append(job_id_partition_key)
-                
-                for i in job_id_key_list:
-                    response = query_dynamo_db_job_status(i)
-                    responses[job_id] = response['Items']
-                all_successful = all(len(items) > 0 and items[0]['job_status'] == 'success' for items in responses.values())
-
-                if all_successful:
-                    print("All job IDs are successful")
-                else:
-                    print("Some job IDs are not successful")
-
-            elif job_status == 'FAIL':
-                print('PAGERDUTY ALERT WILL BE TRIGGERED')
-
-            elif job_status == 'IN_PROGRESS':
-                print('skipped')
-            # job_end_utc_timestamp = new_image['job_end_utc_timestamp']['N']
-            # failure_message = new_image['failure_message']['S']
-            # is_failed_job_retryable = new_image['is_failed_job_retryable']['BOOL']
-            # data_s3_paths_associated = [path['S'] for path in new_image['data_s3_paths_associated']['L']]
+        return {
+            'statusCode': 200,
+            'body': json.dumps(responses)
+        }
 
     except Exception as e:
-        raise e
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
