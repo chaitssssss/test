f0e35f0fdb49d423b0a49e4cd19df3388e43d65f
chaitssssss <chaituthegoodboy@gmail.com>
2024-08-09 16:28:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-@patch('src.lambda_handler.load_yaml')
-@patch('src.lambda_handler.check_mandatory_jobs_success')
-def test_lambda_handler_success(mock_check_mandatory_jobs_success, mock_load_yaml):
-    mock_load_yaml.return_value = config_data
-    mock_check_mandatory_jobs_success.return_value = (True, {})
-    
-    event = {}
-    context = {}
-    response = lambda_handler(event, context)
-assert response['statusCode'] == 200
-    body = json.loads(response['body'])
-    assert body['status'] == 'success'
-
-@patch('src.lambda_handler.load_yaml')
-@patch('src.lambda_handler.check_mandatory_jobs_success')
-def test_lambda_handler_failure(mock_check_mandatory_jobs_success, mock_load_yaml):
-    mock_load_yaml.return_value = config_data
-    mock_check_mandatory_jobs_success.return_value = (False, {})
-event = {}
-    context = {}
-    response = lambda_handler(event, context)
-assert response['statusCode'] == 200
-    body = json.loads(response['body'])
-    assert body['status'] == 'fail'
+else:
+            response["status"] = "failed"
+            response["message"] = "All Mandatory jobs are not successful"
+            return {
+                'statusCode': 500,
+            'body': response
+            }
