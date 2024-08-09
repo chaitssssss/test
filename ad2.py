6709c43d9b2b5ebf58f25083836b59d853988e72
chaitssssss <chaituthegoodboy@gmail.com>
2024-08-09 20:19:56 +0530

    Create ad2.py

---
+++ ad2.py
@@ -0,0 +1,9 @@
+@patch('lambda_function.boto3.client')
+def test_cleanup_cloudwatch_rule(mock_boto_client):
+    mock_cloudwatch = MagicMock()
+    mock_boto_client.return_value = mock_cloudwatch
+    mock_cloudwatch.describe_rule.return_value = {'Name': 'RescheduleLambdaRule'}
+
+    cleanup_cloudwatch_rule()
+    mock_cloudwatch.remove_targets.assert_called_once_with(Rule='RescheduleLambdaRule', Ids=['1'])
+    mock_cloudwatch.delete_rule.assert_called_once_with(Name='RescheduleLambdaRule')
